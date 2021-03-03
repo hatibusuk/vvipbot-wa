ff9778ca7e795b3c6f0d2db3b7c401b35c8c8bc8
hatibusuk <77769758+hatibusuk@users.noreply.github.com>
2021-03-03 10:26:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Wafiq\n' // Nama kamu
+            + 'ORG:MIKUBOT;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=601128692700:+60 11-2869-2700\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
